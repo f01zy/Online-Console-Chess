9751b5a5b7806f19b3a53e2621bac5a50cb19bd7
f01zy <aminovali015@icloud.com>
2025-03-03 19:31:05 +0300

    add sing up logic

--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,145 @@
-#include <chrono>
+#include <cstdlib>
+#include <curl/curl.h>
+#include <curl/easy.h>
+#include <fstream>
 #include <iostream>
-#include <sio_client.h>
-#include <thread>
+#include <string>
 
 using namespace std;
-using namespace sio;
 
-void pong(sio::event &) { cout << "[INFO] pong return" << endl; }
+const string API_URL = "http://localhost:3000/api/v1";
 
-int main() {
-  client h;
+bool validate_password(const string &password, const string &confirm_password) {
+  return password == confirm_password;
+}
 
-  h.set_open_listener([]() { cout << "connected to server." << endl; });
-  h.socket()->on("pong", &pong);
+size_t WriteCallback(void *contents, size_t size, size_t nmemb, void *userp) {
+  ((std::string *)userp)->append((char *)contents, size * nmemb);
+  return size * nmemb;
+}
 
-  h.connect("http://localhost:3000");
+void sign_up(const string &email, const string &username,
+             const string &password) {
+  CURL *curl;
+  CURLcode res;
+  string readBuffer;
 
-  while (true) {
-    cout << "[INFO] send requrest" << endl;
-    h.socket()->emit("ping");
-    std::this_thread::sleep_for(std::chrono::seconds(5));
+  curl = curl_easy_init();
+
+  if (curl) {
+    string url = API_URL + "/auth/register";
+    string fields =
+        "email=" + email + "&username=" + username + "&password=" + password;
+
+    curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+    curl_easy_setopt(curl, CURLOPT_POSTFIELDS, fields.c_str());
+
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
+    curl_easy_setopt(curl, CURLOPT_WRITEDATA, &readBuffer);
+
+    res = curl_easy_perform(curl);
+
+    if (res != CURLE_OK) {
+      std::cerr << "curl_easy_perform() failed: " << curl_easy_strerror(res)
+                << std::endl;
+    } else {
+      std::cout << readBuffer << std::endl;
+    }
+
+    curl_easy_cleanup(curl);
+  }
+}
+
+bool sign_in(const string &username, const string &password) { return false; }
+
+void clear() { system("clear"); }
+
+void auth() {
+  clear();
+
+  setlocale(LC_ALL, "EN_US.UTF-8");
+
+  int choice;
+  string email;
+  string username;
+  string password;
+  string confirm_password;
+
+  cout << "        ===== Chess Online =====        " << endl;
+  cout << "===== Sign In (1) ===== Sign Up (2) =====" << endl;
+  cin >> choice;
+
+  clear();
+  switch (choice) {
+  case 1:
+    cout << "Enter your email: ";
+    cin >> email;
+
+    cout << "Enter your password: ";
+    cin >> password;
+
+    if (sign_in(email, password)) {
+      cout << "Sign in case result" << endl;
+    }
+
+    break;
+
+  case 2: {
+    bool passwords_match = false;
+    int attempts = 3;
+
+    cout << "Change email: ";
+    cin >> email;
+
+    cout << "Change username: ";
+    cin >> username;
+
+    do {
+      cout << "Create password: ";
+      cin >> password;
+
+      cout << "Confirm password: ";
+      cin >> confirm_password;
+
+      if (validate_password(password, confirm_password)) {
+        passwords_match = true;
+        sign_up(email, username, password);
+      }
+
+      else {
+        clear();
+
+        attempts--;
+        cout << "Passwords do not match! Remaining attempts: ";
+        cout << attempts << endl;
+
+        if (attempts <= 0) {
+          clear();
+          auth();
+        }
+      }
+    } while (!passwords_match && attempts > 0);
+
+    break;
   }
 
-  h.sync_close();
+  default:
+    cout << "Invalid choice!" << endl;
+    break;
+  }
 
-  return 0;
+  curl_global_cleanup();
+}
+
+int main() {
+  ifstream tokenFile("token.txt");
+  string token;
+
+  getline(tokenFile, token);
+
+  if (token.empty()) {
+    auth();
+  }
 
   return 0;
 }
